7982816736ee1f9721a62a092923dcebc01e8e35
Parkbb <pyj9088@gmail.com>
2019-10-02 16:29:01 +0900

    d

--- src/com/iu/control/SalgradeController.java
+++ src/com/iu/control/SalgradeController.java
@@ -11,46 +11,72 @@ import com.iu.view.SalView;
 public class SalgradeController {
 
 	private Scanner sc;
+	private SalgradeDAO salDAO;
+	private SalgradeDTO salDTO;
+	private SalInput salIn;
+	private SalView view;
 
-	
+	public SalgradeController() {
+		sc = new Scanner(System.in);
+		salDAO = new SalgradeDAO();
+		salIn = new SalInput();
+		view = new SalView();
+		salDTO = new SalgradeDTO();
+
+	}
+
+
 	public void start() {
 		boolean check = true;
-		sc = new Scanner(System.in);
-		SalgradeDAO salDAO = new SalgradeDAO();
-		SalInput salIn = new SalInput();
-		SalView view = new SalView();
 		ArrayList<SalgradeDTO> ar = null;
-		SalgradeDTO salDTO = null;
 		while(check) {
 			System.out.println("1. 전체급여등급조회");
 			System.out.println("2. 개별급여등급조회");
 			System.out.println("3. 급 여 등 급 추 가");
 			System.out.println("4. 급 여 등 급 삭 제");
+			System.out.println("5. 종                료");
+
 			int select = sc.nextInt();
-			
+
 			switch(select) {
 			case 1:
 				ar = salDAO.getselectList();
-				view.view(ar);
+				if(ar.size()>0) {
+					view.view(ar);					
+				}
 				break;
 			case 2:
 				select = salIn.gradeInput();
 				salDTO = salDAO.getselectOne(select);
-				view.view(salDTO);
+				if(salDTO != null) {
+
+					view.view(salDTO);
+				}else {
+					view.view("없는 등급");
+				}
 				break;
 			case 3:
 				salDTO = salIn.insert();
-				salDAO.insert(salDTO);
+				select = salDAO.insert(salDTO);
+				String s = "등급 정보 입력 실패";
+				if(select >0) {
+					s = "등급 정보 입력 성공";
+				}
+				view.view(s);
 				break;
 			case 4:
 				int grade = salIn.delete();
-				salDAO.delete(grade);
+				grade = salDAO.delete(grade);
+				s = "등급 삭제 실패";
+				if(grade >0) {
+					s = "등급 삭제 성공";
+				}
 			default :
 				check = false;
 				break;
-				
+
 			}
 		}
-		
+
 	}
 }
